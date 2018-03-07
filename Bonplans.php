67846a0d8e33d0172023e6e6e41181b9882eb371
Naash <hasan.taskin.pro@gmail.com>
2018-03-07 15:38:15 +0100

    first commit

---
+++ Bonplans.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+<!-- Bootstrap --> <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <meta charset="utf-8">
+    <title>Les bons plans</title>
+  </head>
+  <body>
+
+    <?php include 'BonPlansHeader.php'; ?>
+
+
+
+
+
+
+
+
+  </body>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</html>
