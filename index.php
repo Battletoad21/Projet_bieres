398ae995919bac856ff7a05bff57b4fa241fd250
Hery Rasamimanana <hery.rasamimanana@gmail.com>
2018-03-07 14:14:38 +0100

    fichiers de base

---
+++ index.php
@@ -0,0 +1,199 @@
+<<!DOCTYPE html>
+<html>
+
+  <head>
+
+    <meta charset="utf-8">
+    <title>MonSite</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="style_index.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  </head>
+
+  <body>
+
+    <!--          HEADER           -->
+    <?php include ('header.php'); ?>
+    <!--          HEADER END           -->
+
+    <!--          SECTION          -->
+    
+    <section>
+
+      <!--          Caroussel          -->
+      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+          <div class="item active">
+            <a href="#"><img src="img/slide01.png"></a>
+            <div class="carousel-caption">
+              <H2>BIERE BRUNE DU MOIS</H2>
+            </div>
+          </div>
+          <div class="item">
+            <a href="#"><img src="img/slide02.png"></a>
+            <div class="carousel-caption">
+              <H2>BIERE BLONDE DU MOIS</H2>
+            </div>
+          </div>
+          <div class="item">
+            <a href="#"><img src="img/slide03.png"></a>
+            <div class="carousel-caption">
+              <H2>BIERE BLANCHE DU MOIS</H2>
+            </div>
+          </div>
+        </div>
+
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+      <!--          Caroussel end        -->
+
+      <!--          Thumbnails        -->
+
+      <div class="row select-thumb">
+
+        <div class="col-md-3">
+          <div class="thumbnail">
+            <img src="img/beer.png" alt="...">
+            <div class="caption">
+              <h2>BRASSERIE 3 MÂTS</h2>
+              <h3>Bière La Kraken</h3>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="thumbnail">
+            <img src="img/beer.png" alt="...">
+            <div class="caption">
+              <h2>BRASSERIE 3 MÂTS</h2>
+              <h3>Bière La Kraken</h3>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="thumbnail">
+            <img src="img/beer.png" alt="...">
+            <div class="caption">
+              <h2>BRASSERIE 3 MÂTS</h2>
+              <h3>Bière La Kraken</h3>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="thumbnail">
+            <img src="img/beer.png" alt="...">
+            <div class="caption">
+              <h2>BRASSERIE 3 MÂTS</h2>
+              <h3>Bière La Kraken</h3>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="thumbnail">
+            <img src="img/beer.png" alt="...">
+            <div class="caption">
+              <h2>BRASSERIE 3 MÂTS</h2>
+              <h3>Bière La Kraken</h3>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="thumbnail">
+            <img src="img/beer.png" alt="...">
+            <div class="caption">
+              <h2>BRASSERIE 3 MÂTS</h2>
+              <h3>Bière La Kraken</h3>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="thumbnail">
+            <img src="img/beer.png" alt="...">
+            <div class="caption">
+              <h2>BRASSERIE 3 MÂTS</h2>
+              <h3>Bière La Kraken</h3>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="thumbnail">
+            <img src="img/beer.png" alt="...">
+            <div class="caption">
+              <h2>BRASSERIE 3 MÂTS</h2>
+              <h3>Bière La Kraken</h3>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+          </div>
+        </div>
+
+      </div>
+
+      <!--          Thumbnails end        -->
+
+      <!--          Specialist        -->
+
+      <div class="row">
+        <div class="col-md-12">
+          <div class="thumbnail specialist">
+            <img src="img/beer.png" alt="...">
+            <div class="caption">
+              <h3>Le mot de notre spécialiste</h3>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.<br><br><strong>Kyle Brace</strong></p>
+              <p><a href="#" class="btn btn-primary" role="button">Nous contacter</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!--          Specialist end        -->
+
+    </section>
+
+
+    <!--        SECTION END        -->
+ 
+
+    <!--          FOOTER           -->
+    <?php include ('footer.php'); ?>
+    <!--         FOOTER END        -->
+
+
+<!--         Scripts        -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<!--       Scripts end       -->
+
+  </body>
+
+</html>
