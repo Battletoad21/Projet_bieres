e8bf4c16dc7859917a51804227eebdd09daad14d
Hery Rasamimanana <hery.rasamimanana@gmail.com>
2018-03-09 14:02:58 +0100

    Modification FOOTER

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <!--          FOOTER           -->
     <footer class="container-fluid">
       <div class="row row-centered">
-        <div class="col-md-12 col-centered link_foot_one"><a href="#">Facebook</a><a href="#">Twitter</a></div>
+        <div class="col-md-12 col-centered link_foot_one"><a href="http://facebook.com"><img src="img/index.png" alt="..."></a><a href="http://twitter.com"><img src="img/index.jpeg" alt="..."></a></div>
       </div>
       <div class="row row-centered">
         <div class="col-md-12 col-centered link_foot_two"><a href="#">Mentions légales</a></div>
