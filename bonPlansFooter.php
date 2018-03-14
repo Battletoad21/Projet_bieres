97a56f477d4dd7a1c85e70d3ff9b4fd509b4872b
Naash <hasan.taskin.pro@gmail.com>
2018-03-14 16:24:06 +0100

    COMMIT C GOOD

---
+++ bonPlansFooter.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="style_index.css">
+    <!--          FOOTER           -->
+    <footer class="container-fluid">
+      <div class="row row-centered">
+        <div class="col-md-12 col-centered link_foot_one"><a href="#">Facebook</a><a href="#">Twitter</a></div>
+      </div>
+      <div class="row row-centered">
+        <div class="col-md-12 col-centered link_foot_two"><a href="#">Mentions légales</a></div>
+      </div>
+      <div class="row row-centered">
+        <div class="col-md-12 col-centered link_foot_two"><a href="#logo">BACK TO TOP</a></div>
+      </div>
+    </footer>
+    <!--         FOOTER END        -->
+ 
