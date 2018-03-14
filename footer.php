73c9a24daf3468e8326378ee5c84c0a6244e0d9a
Hery Rasamimanana <hery.rasamimanana@gmail.com>
2018-03-14 17:21:55 +0100

    Modification liens footer

--- footer.php
+++ footer.php
@@ -4,10 +4,10 @@
 
       <div class="container-fluid first_bloc">
         <div class="row row-centered">
-          <div class="col-md-12 col-centered link_foot_one"><a href="http://facebook.com"><img src="img/fb.png" alt="..."></a><a href="http://twitter.com"><img src="img/tw.png" alt="..."></a></div>
+          <div class="col-md-12 col-centered link_foot_one"><a target = "_blank" href="https://www.facebook.com/kyle.brace.54"><img src="img/fb.png" alt="..."></a><a target = "_blank" href="http://twitter.com"><img src="img/tw.png" alt="..."></a></div>
         </div>
         <div class="row row-centered">
-          <div class="col-md-12 col-centered link_foot_two"><a href="mentions_legales.php">Mentions légales</a></div>
+          <div class="col-md-12 col-centered link_foot_two"><a target = "_blank" href="mentions_legales.php">Mentions légales</a></div>
         </div>
         <div class="row row-centered">
           <div class="col-md-12 col-centered link_foot_two"><p>© Wild Code School 2018 - Kyle, Hasan, Hery</p></div>
