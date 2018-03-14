defd7cf32d35ac038aaf6fb2f998bc19d9bbdeda
Kyle Brace <kylebrace1@gmail.com>
2018-03-14 15:05:46 +0100

    commit

--- atypique.php
+++ atypique.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 
+
+
   <head>
 
     <meta charset="utf-8">
@@ -8,8 +10,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="atypique.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
+    <link href="https://fonts.googleapis.com/css?family=Cinzel|Electrolize" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Megrim" rel="stylesheet">
 
   </head>
 
@@ -21,15 +23,11 @@
 
 
       <!--          Logo          -->
-      <div id="logo">
-        <a href="#"><img src="http://via.placeholder.com/350x150"></a>
-      </div>
+
       <!--          Logo end         -->
 
       <!--          Menu          -->
-<?p
-
-</header>')
+<?php include('header.php'); ?>
       <!--          Menu end         -->
 
 
@@ -54,7 +52,7 @@
                         <img src="https://i0.wp.com/www.pokaa.fr/wp-content/uploads/2016/11/bar-barberousse-strasbourg4.jpg" alt="Bar" class="responsive" >
                         <figcaption class="caption">
                             <h3>Le Barberousse</h3>
-                            <p>Ahoy, matey! Si la mer vous manque, allez au barberousse pour vous noyer dans de l'eau améliorée (avec modération évidement, oupas). Le Barberousse vous offre un cadre original sur le theme de la piraterie. Encore un peu plus de blablabla pour voir si ça marche</p>
+                            <p>Ahoy, matey! Si la mer vous manque, allez au barberousse pour vous noyer dans de l'eau améliorée (avec modération évidement, oupas). Le Barberousse vous offre un cadre original sur le theme de la piraterie. Bonne bière moussaillon! Mais va passez le balai avant.</p>
                         </figcaption>
                     </figure>
                 </div>
@@ -74,7 +72,7 @@
                                         <img src="http://www.barlalchimiste.com/img/galerie/b8db5ee0649b328fb591aa2c3136455c.jpg" alt="Bar" class="responsive">
                                         <figcaption class="caption">
                                             <h3>L'Achimiste</h3>
-                                            <p>Dans cette cabane qui n'est pas au fond du jardin, on vous propose principalement des cocktails très ... originaux. Mais la bière répond présente également. Une bonne ambiance pour une bonne soirée.</p>
+                                            <p>Le Merlin des cocktails! Spécialisé dans les cocktails, l'alchimiste est l'endroit pour tester de nouvelles potions. L'endroit, bien que petit, offre une ambiance chaleureuse.</p>
                                         </figcaption>
                                     </figure>
                                 </div>
@@ -84,7 +82,7 @@
                                                 <img src="https://pbs.twimg.com/media/DIGkq1pXgAESzeq.jpg" alt="Bar" class="responsive">
                                                 <figcaption class="caption">
                                                     <h3>Le Délirium Café</h3>
-                                                    <p>Moonshine Dans cette cabane qui n'est pas au fond du jardin, on vous propose principalement des cocktails très ... originaux. Mais la bière répond présente également. Une bonne ambiance pour une bonne soirée.</p>
+                                                    <p>Willy Wonka a sa chocolaterie. Il peut se la garder car nous avons le Délirium. Des dizaines et dizaines de bières avec une ambiance au top!</p>
                                                 </figcaption>
                                             </figure>
                                         </div>
@@ -94,7 +92,7 @@
                                                         <img src="https://lescookines.files.wordpress.com/2015/01/phonographe-strasbourg.jpg?w=940" alt="Bar" class="responsive">
                                                         <figcaption class="caption">
                                                             <h3>Le Phonographe</h3>
-                                                            <p>Dans cette cabane qui n'est pas au fond du jardin, on vous propose principalement des cocktails très ... originaux. Mais la bière répond présente également. Une bonne ambiance pour une bonne soirée.</p>
+                                                            <p>Le phonographe, situé juste à coté de la place saint Etienne vous englobe d'une ambiance entre le musique et le houblon. Vous pourrez également faire du rétro gaming. Il faut s'y prendre tot pour y avoir une place car l'endroit reste malgré tout petit.</p>
                                                         </figcaption>
                                                     </figure>
                                                 </div>
@@ -104,7 +102,7 @@
                                                                 <img src="https://i2.wp.com/www.pokaa.fr/wp-content/uploads/2016/09/Fat-Black-Pussy-Cat.jpg" alt="Bar" class="responsive">
                                                                 <figcaption class="caption">
                                                                     <h3>Le Fat Black Pussycat</h3>
-                                                                    <p>Dans cette cabane qui n'est pas au fond du jardin, on vous propose principalement des cocktails très ... originaux. Mais la bière répond présente également. Une bonne ambiance pour une bonne soirée.</p>
+                                                                    <p>Si vous aimez l'inconnu, le fat black! La sélection de bière y reste correct mais le plus de cet endroit est réellement la variété. Le jeudi 90s, le vendredi disco, le samedi rock.</p>
                                                                 </figcaption>
                                                             </figure>
                                                         </div>
@@ -119,10 +117,11 @@
               <div class="row box-kyle">
                      <div class="col-md-12">
                        <div class="thumbnail specialist">
-                         <img src="img/kyle_exp.jpg" alt="...">
+                         <img src="lampe.jpg" alt="...">
                          <div class="caption text-kyle">
                            <h3>Le mot de notre spécialiste</h3>
-                           <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>
+                           <p>La bière c'est bien, mais il n'y a pas que ça dans la vie. J'ai donc décidé de tester un cocktail. Et voila celui
+                           qui a retenu mon attention. Servi dans une ampoule (quelle idée lumineuse), se cocktail à base de rhum est léger, et bon! A prendre pour faire une pause entre deux bières! </p>
                            <p><a href="#" class="btn btn-primary contact-kyle" role="button">Nous contacter</a></p>
                          </div>
                        </div>
@@ -148,17 +147,7 @@
 
 
     <!--          FOOTER           -->
-    <footer class="container-fluid">
-      <div class="row row-centered">
-        <div class="col-md-12 col-centered link_foot_one"><a href="#">Facebook</a><a href="#">Twitter</a></div>
-      </div>
-      <div class="row row-centered">
-        <div class="col-md-12 col-centered link_foot_two"><a href="#">Mentions légales</a></div>
-      </div>
-      <div class="row row-centered">
-        <div class="col-md-12 col-centered link_foot_two"><a href="#">BACK TO TOP</a></div>
-      </div>
-    </footer>
+
     <!--         FOOTER END        -->
 
 
@@ -167,6 +156,8 @@
 <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <!--       Scripts end       -->
+
+<?php include('footer.php'); ?>
 
   </body>
 
---
+++ footer.php
@@ -0,0 +1,14 @@
+
+    <!--          FOOTER           -->
+    <footer class="container-fluid">
+      <div class="row row-centered">
+        <div class="col-md-12 col-centered link_foot_one"><a href="#">Facebook</a><a href="#">Twitter</a></div>
+      </div>
+      <div class="row row-centered">
+        <div class="col-md-12 col-centered link_foot_two"><a href="#">Mentions légales</a></div>
+      </div>
+      <div class="row row-centered">
+        <div class="col-md-12 col-centered link_foot_two"><a href="#logo">BACK TO TOP</a></div>
+      </div>
+    </footer>
+    <!--         FOOTER END        -->
