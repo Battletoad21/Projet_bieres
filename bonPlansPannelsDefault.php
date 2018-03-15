a4f426a3657469b4f5fa96d0950f3f67c1f24bd3
Naash <hasan.taskin.pro@gmail.com>
2018-03-15 11:42:22 +0100

    Pannels without spécialiste

--- bonPlansPannelsDefault.php
+++ bonPlansPannelsDefault.php
@@ -8,13 +8,14 @@
         <div class="row">
           <div class="col-md-4 col-sm-12 col-xs-12">
             <figure class="text-center">
-              <img src="https://static1.squarespace.com/static/555b4653e4b09b331f3a25ed/t/5729221920c647e5196d288e/1520435807836/?format=1500w">
+            <a href="https://www.lesberthom.com/les-bars/les-berthom-a-strasbourg/" target="_blank"><img src="https://www.lesberthom.com/wp-content/uploads/2013/06/bar-biere-berthom-tours.jpg"></a>
             </figure>
           </div>
           <div class="col-md-8 col-sm-12 col-xs-12">
-            <span class="label label-default pull-right"><i class="glyphicon glyphicon-comment"></i>50</span>
-            <h4>Raymond Dragon</h4>
-            <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            <a href="https://www.lesberthom.com/les-bars/les-berthom-a-strasbourg/" target="_blank"><h4>Les Berthom</h4></a>
+            <p> Depuis 2002, le bar à bières Les Berthom fait le bonheur des Strasbourgeois avec sa variété de boissons admirables et son emplacement idéal situé à 100 mètres de la Place Gutenberg.
+              Le personnel est à votre petit soin pour vous proposer la meilleure bière selon vos envies.
+              En cas de petite faim, "Les Berthom" propose également une carte de snacks. Pour faire des économies, privilégiez l'Happy Hour du dimanche au vendredi, de 19h00 à 21h00.
           </div>
         </div>
       </article>
@@ -23,13 +24,98 @@
         <div class="row">
           <div class="col-md-4 col-sm-12 col-xs-12">
             <figure class="text-center">
-              <img src="https://static1.squarespace.com/static/555b4653e4b09b331f3a25ed/t/5729221920c647e5196d288e/1520435807836/?format=1500w">
+            <a href="http://www.obrien-strasbourg.com/fr/" target="_blank"><img src="https://media-cdn.tripadvisor.com/media/photo-s/05/49/86/32/o-brien.jpg"></a>
             </figure>
           </div>
           <div class="col-md-8 col-sm-12 col-xs-12">
-            <span class="label label-default pull-right"><i class="glyphicon glyphicon-comment"></i>50</span>
-            <h4>Raymond Dragon</h4>
-            <p> Lorem ipsum dolor sit a met, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            <a href="http://www.obrien-strasbourg.com/fr/" target="_blank"><h4>Le O'Brien Irish Pub</h4></a>
+            <p> Le O'Brien Irish Pub est le bar irlandais par excellence de Strasbourg.
+                Avec sa variété de bières, sa déco atypique et ses petits prix, vous allez adorer y passer vos soirée.
+                Le demi est à 2,50 € et la pinte à 3,50 € en Happy Hour (16h00 à 20h00).
+                Mais pour une soirée entre amis originale, tentez l'expérience du "Own Pint", une table où vous pouvez tirer votre propre bière à partir de 8 € le litre (pour 10 litres ou plus).
+                Ce bar propose également des snacks, sandwiches et burgers avec un menu étudiant à 7,50 € (valable 7j/7 tout au long de la journée) comprenant une boisson, un burger, des frites et une salade.
+          </div>
+        </div>
+      </article>
+
+      <article>
+        <div class="row">
+          <div class="col-md-4 col-sm-12 col-xs-12">
+            <figure class="text-center">
+              <a href="http://www.jimmysbar.fr/" target="_blank"><img src="https://s3-media1.fl.yelpcdn.com/bphoto/vRpkZRzZmY34M2xjxGHJ0g/ls.jpg"></a>
+            </figure>
+          </div>
+          <div class="col-md-8 col-sm-12 col-xs-12">
+          <a href="http://www.jimmysbar.fr/" target="_blank"><h4> Le Jimmy's Bar</h4></a>
+              <p> Avec les diffusions des grands évenements sportifs, le "Jimmy's Bar" sera le repère des amateurs de sports, mais aussi de musique : des groupes de musique et des Dj    se produisent dans ce bar plusieurs soirs par semaine.
+                  À chaque soir son thème, sans oublier une carte variée avec des prix très intéressants : 3 € pour les bières pression et de 4 à 9 € pour les cocktails (sans alcool).
+          </div>
+        </div>
+      </article>
+
+      <article>
+        <div class="row">
+          <div class="col-md-4 col-sm-12 col-xs-12">
+            <figure class="text-center">
+            <a href="http://www.foxbar.fr/" target="_blank"><img src="http://www.foxbar.fr/images/what-the-fox-12.jpg"></a>
+            </figure>
+          </div>
+          <div class="col-md-8 col-sm-12 col-xs-12">
+            <a href="http://www.foxbar.fr/" target="_blank"><h4>What the fox</h4></a>
+            <p> The place to be pour une soirée entre amis à déguster de la bonne bière et grignoter des croques,  des flamms ... Les happy hours sont très avantageux.
+                La pinte au prix de la demi. Le midi ils proposent de bons outils plats du jours.
+                La carte des bières est très bien fournie avec toujours des nouveautés !
+          </div>
+        </div>
+      </article>
+
+      <article>
+        <div class="row">
+          <div class="col-md-4 col-sm-12 col-xs-12">
+            <figure class="text-center">
+            <a href="https://www.lecroquebedaine.fr/" target="_blank"><img src="https://i1.wp.com/www.pokaa.fr/wp-content/uploads/2017/01/croque-bedaine-strasbourg2.jpg?fit=1200%2C800&ssl=1"></a>
+            </figure>
+          </div>
+          <div class="col-md-8 col-sm-12 col-xs-12">
+          <a href="https://www.lecroquebedaine.fr/" target="_blank"><h4>Le Croc'Bedaine</h4></a>
+            <p> Un petit bar/resto perdu dans une petite rue.
+                Mais quelle bonne surprise ! La déco un peu vieillotte donne une ambiance tranquillement décalée.
+                Les croques sont très bons et originaux, on apprécie le petit concours de recette chaque mois.
+                La recette la plus "likée" sera réalisée par le chef. Ca fait un peu de nouveauté et c'est pas de refus !
+                Attention aux portions de frites qui sont vraiment balèze ! Lorsque j'y suis allée avec une amie , une portion pour deux aurait parfaitement suffit.
+                Il y a happy hour sur toutes les boissons de 17h à 20h30.Votre commande arrive sur une plaquette en bois, façon planche à découper, et c'est convivial à souhait.
+          </div>
+        </div>
+      </article>
+
+      <article>
+        <div class="row">
+          <div class="col-md-4 col-sm-12 col-xs-12">
+            <figure class="text-center">
+            <a href="http://www.academiedelabiere.com/" target="_blank"><img src="https://media-cdn.tripadvisor.com/media/photo-s/06/b0/6d/7e/academie-de-la-biere.jpg"></a>
+            </figure>
+          </div>
+          <div class="col-md-8 col-sm-12 col-xs-12">
+            <a href="http://www.academiedelabiere.com/" target="_blank"><h4>Académie de la Bière</h4></a>
+            <p> Par où commencer ? La terrasse est formidable en été, l'intérieur du bar a franchement de la gueule avec son côté "cabane". Le choix de bière est très impressionnant (autant pour les pressions que pour les bières en bouteille), l'équipe du bar est très agréable et compétente. Pour manger c'est pas mal aussi avec des prix très raisonnable.
+            Alors voilà, comme son nom l'indique, ici, l'intérêt principal du bar, c'est la bière.
+            Au final, à Strasbourg, des bars pour boires des bières qui sortent un peu de l'ordinaire, il n'y en a pas tant que ça. De fait, ce bar possède un avantage que la quasi-totalité des bars de la ville ne possède pas.
+            ça pourrait s'arrêter là, mais non, au delà du "concept", le bar possède sa petite ambiance bien à lui: quand on est à l'Académie de la bière, on est là et pas ailleurs.
+            Il y a quelque chose de particulier et pour ça: allez y faire un tour, vous ne serez pas déçu. Une valeur sure à Strasbourg.
+          </div>
+        </div>
+      </article>
+
+      <article>
+        <div class="row">
+          <div class="col-md-4 col-sm-12 col-xs-12">
+            <figure class="text-center">
+            <a href="" target="_blank"><img src="imge/kyle.png"></a>
+            </figure>
+          </div>
+          <div class="col-md-8 col-sm-12 col-xs-12">
+            <h4>Le bons plans du spécialiste !</h4>
+            <p> Notre spécialiste vous conseil
           </div>
         </div>
       </article>
